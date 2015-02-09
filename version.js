a96cce394ec926bf87bd20691970392d5da5169a
senro <passtome@foxmail.com>
2015-02-09 10:25:28 +0800

    wn v0.0.14

--- version.js
+++ version.js
@@ -1,14 +1,27 @@
 module.exports = function(){
     var content = [
-         'v' + fis.cli.info.version,
-         ' **       **'.red.bold+'    ****     **'.green.bold,
-         '/**      /**'.red.bold+'   /**/**   /**'.green.bold,
-         '/**   *  /**'.red.bold+'   /**//**  /**'.green.bold,
-         '/**  *** /**'.red.bold+'   /** //** /**'.green.bold,
-         '/** **/**/**'.red.bold+'   /**  //**/**'.green.bold,
-         '/**** //****'.red.bold+'   /**   //****'.green.bold,
-         '/**/   ///**'.red.bold+'   /**    //***'.green.bold,
-         '//       // '.red.bold+'   //      /// '.green.bold
+    'v' + fis.cli.info.version,                                             
+    '               ,               '.yellow.bold,      
+    '              7u   .M.         '.yellow.bold,      
+    '              q,   OL          '.yellow.bold,      
+    '             i8   qG           '.yellow.bold,      
+    '            .@: ,0@,           '.yellow.bold,      
+    '           :0U:ri1r            '.yellow.bold,      
+    '          7B:qu rB:            '.yellow.bold,      
+    '         8@.   rMZBB@OY        '.yellow.bold,      
+    '       7B@          :N@@k      '.yellow.bold,      
+    '      kBO              j@B:    '.yellow.bold,      
+    '     2BG     vM@@BB@P.  .@B,   '.yellow.bold,      
+    '    ,B@    r@Br    .qBP  :@M   '.yellow.bold,      
+    '    U@v   Y@U  :2Fr  ,BN  E@:  '.yellow.bold,      
+    '    G@,  .@k  BBLrOB, 2B. Y@7  '.yellow.bold,      
+    '    P@.  UB: 5@    @. O@  X@i  '.yellow.bold,      
+    '    :B7  i@u :@7     X@r  @B   '.yellow.bold,      
+    '     MB   M@. :@BE1EB@.  @B:   '.yellow.bold,      
+    '      @@   M@U  .r7:   j@@,    '.yellow.bold,      
+    '       G@i  i@@B7i:iu@B@F      '.yellow.bold,      
+    '        ,ZP:  ,UMB@B@1:        '.yellow.bold,      
+    '           i:                  '.yellow.bold
     ].join('\n');
     console.log(content);
 };
