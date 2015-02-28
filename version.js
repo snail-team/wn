cebd519b378c54dfd3075f06995ef1ac1e1378c0
senro <passtome@foxmail.com>
2015-02-28 17:12:22 +0800

    add readme link

--- version.js
+++ version.js
@@ -1,27 +1,21 @@
 module.exports = function(){
     var content = [
-    'v' + fis.cli.info.version,                                             
-    '               ,               '.yellow.bold,      
-    '              7u   .M.         '.yellow.bold,      
-    '              q,   OL          '.yellow.bold,      
-    '             i8   qG           '.yellow.bold,      
-    '            .@: ,0@,           '.yellow.bold,      
-    '           :0U:ri1r            '.yellow.bold,      
-    '          7B:qu rB:            '.yellow.bold,      
-    '         8@.   rMZBB@OY        '.yellow.bold,      
-    '       7B@          :N@@k      '.yellow.bold,      
-    '      kBO              j@B:    '.yellow.bold,      
-    '     2BG     vM@@BB@P.  .@B,   '.yellow.bold,      
-    '    ,B@    r@Br    .qBP  :@M   '.yellow.bold,      
-    '    U@v   Y@U  :2Fr  ,BN  E@:  '.yellow.bold,      
-    '    G@,  .@k  BBLrOB, 2B. Y@7  '.yellow.bold,      
-    '    P@.  UB: 5@    @. O@  X@i  '.yellow.bold,      
-    '    :B7  i@u :@7     X@r  @B   '.yellow.bold,      
-    '     MB   M@. :@BE1EB@.  @B:   '.yellow.bold,      
-    '      @@   M@U  .r7:   j@@,    '.yellow.bold,      
-    '       G@i  i@@B7i:iu@B@F      '.yellow.bold,      
-    '        ,ZP:  ,UMB@B@1:        '.yellow.bold,      
-    '           i:                  '.yellow.bold
+    'v' + fis.cli.info.version,                                   
+     '             .i    i.          '.yellow.bold,      
+     '             Yv   78           '.yellow.bold,      
+     '            :8   uM.           '.yellow.bold,      
+     '           rM: :qM             '.yellow.bold,      
+     '         :0uXL:vMi             '.yellow.bold,      
+     '       :M@: . :2S18OM1:        '.yellow.bold,      
+     '     :B@J           .UB@L      '.yellow.bold,      
+     '    LB@     v5F2u1FL   ;@B.    '.yellow.bold,      
+     '   iBB    N@U. ..  ;B@   @B    '.yellow.bold,      
+     '   Z@,   B@  rqjjNq  O@  L@7   '.yellow.bold,      
+     '   P@.  u@: j@    O  @B  OBi   '.yellow.bold,      
+     '    @@   B@  ZBv::iL@k  PBX    '.yellow.bold,      
+     '     @B.  M@u.,7vvr:  j@@r     '.yellow.bold,      
+     '      rE2. .kBOPqk0O@BG:       '.yellow.bold,      
+     '        .:i     .,..           '.yellow.bold                                        
     ].join('\n');
     console.log(content);
 };
